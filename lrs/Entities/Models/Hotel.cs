67be77e12efbabfe939f9e6b798be7fb499061ff
Ivan Saltykov <92875700+IvanSaltykov@users.noreply.github.com>
2022-09-24 19:57:00 +0300

    лр2

---
+++ lrs/Entities/Models/Hotel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Models
+{
+    public class Hotel
+    {
+        [Required]
+        [Column("HotelId")]
+        public Guid Id { get; set; }
+
+        [Required]
+        [ForeignKey(nameof(City))]
+        public string CityId { get; set; }
+
+        [Required]
+        [MaxLength(20, ErrorMessage = "Максимальное количество символов 20")]
+        public string Name { get; set; }
+        public City city { get; set; }
+    }
+}
