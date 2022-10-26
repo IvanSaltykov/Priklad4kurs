d4d9f2c4c4862f1a6949336b8c9296bd9187a32f
Ivan Saltykov <92875700+IvanSaltykov@users.noreply.github.com>
2022-10-26 21:27:59 +0300

    Лабораторная работа 9

--- lrs/Repository/EmployeeRepository.cs
+++ lrs/Repository/EmployeeRepository.cs
@@ -9,6 +9,7 @@ using Contracts;
 using System.ComponentModel.Design;
 using Microsoft.EntityFrameworkCore;
 using Entities.RequestFeatures;
+using Repository.Extensions;
 
 namespace Repository
 {
@@ -33,12 +34,17 @@ namespace Repository
                 e => e.CompanyId.Equals(companyId) && e.Id.Equals(id), trackChanges
             ).SingleOrDefaultAsync();
 
+        //public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
+        //{
+        //    var employees = await FindByCondition(
+        //        e => e.CompanyId.Equals(companyId) && (e.Age >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge), 
+        //        trackChanges
+        //    ).OrderBy(e => e.Name).ToListAsync(); 
+        //    return PagedList<Employee>.ToPagedList(employees, employeeParameters.PageNumber, employeeParameters.PageSize);
+        //}
         public async Task<PagedList<Employee>> GetEmployeesAsync(Guid companyId, EmployeeParameters employeeParameters, bool trackChanges)
         {
-            var employees = await FindByCondition(
-                e => e.CompanyId.Equals(companyId) && (e.Age >= employeeParameters.MinAge && e.Age <= employeeParameters.MaxAge), 
-                trackChanges
-            ).OrderBy(e => e.Name).ToListAsync(); 
+            var employees = await FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges).FilterEmployees(employeeParameters.MinAge, employeeParameters.MaxAge).Search(employeeParameters.SearchTerm).OrderBy(e => e.Name).ToListAsync();
             return PagedList<Employee>.ToPagedList(employees, employeeParameters.PageNumber, employeeParameters.PageSize);
         }
     }
